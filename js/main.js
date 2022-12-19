c2c6c5ba26db3966632ccb5203e258f363762248
Lucas Santana Figueiredo <santanafx@hotmail.com>
2022-12-19 10:18:40 -0300

    Modificando o código para ficar mais dinâmico

--- js/main.js
+++ js/main.js
@@ -1,19 +1,16 @@
-const robotron = document.querySelector("#robotron");
-const somar = document.querySelector("#somar");
-const subtrair = document.querySelector("#subtrair");
-const braco = document.querySelector("#braco");
 const controle = document.querySelectorAll(".controle");
 
 controle.forEach((elemento) => {
   elemento.addEventListener("click", (evento) => {
-    manipulandoDados(evento.target.textContent);
+    manipulandoDados(evento.target.textContent, evento.target.parentNode);
   });
 });
 
-const manipulandoDados = (operacao) => {
+const manipulandoDados = (operacao, controle) => {
+  const peca = controle.querySelector(".controle-contador");
   if (operacao == "-") {
-    braco.value = parseInt(braco.value) - 1;
+    peca.value = parseInt(peca.value) - 1;
   } else {
-    braco.value = parseInt(braco.value) + 1;
+    peca.value = parseInt(peca.value) + 1;
   }
 };
